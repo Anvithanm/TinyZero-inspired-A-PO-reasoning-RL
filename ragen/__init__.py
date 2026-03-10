261d9728a9bb73269f2101f52eeab156137cdbcc
anvithanm <anvithanm22@gmail.com>
2026-03-09 17:40:12 -0700

    Updating the readme file

--- ragen/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-RAGEN: Reinforcement learning for Agent GeNeration
-Multi-turn extension of TinyZero
-"""
-from .agent_trainer import MultiTurnAPOTrainer
-from .environments import WebShopEnvironment  # ← Changed back
-
-__all__ = ['MultiTurnAPOTrainer', 'WebShopEnvironment']
--- ragen/environments/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Environments package for RAGEN
-"""
-from .base import MultiTurnEnvironment
-from .webshop import WebShopEnvironment
-
-__all__ = ['MultiTurnEnvironment', 'WebShopEnvironment']
